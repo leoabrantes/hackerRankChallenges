a85838bc50b701ad62fad3ae92f5e961e8d554f1
Leo Abrantes <leoabrantesmelo@gmail.com>
2024-07-19 11:41:28 -0300

    JavaLoops2

--- src/easy/JavaLoops2.java
+++ src/easy/JavaLoops2.java
@@ -7,34 +7,36 @@ public class JavaLoops2 {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
-		
-		Scanner sc = new Scanner(System.in); 
-		
+
+		Scanner sc = new Scanner(System.in);
+
 		int q = sc.nextInt();
-		
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int n = sc.nextInt();
-		int[] s = new int[n];
-		
-		for(int i=0;i<q;i++) {
-			for(int j=0;j<n;j++) {
-				s[]
-				
-				
-				
-				
-				
+
+		int[][] v = new int[q][3];
+
+		for (int i = 0; i < q; i++) {
+			for (int j = 0; j < 3; j++) {
+				v[i][j] = sc.nextInt();
 			}
 		}
-	
-		
-		
-		
-		
+
+		for (int ii = 0; ii < q; ii++) {
+
+			int s = (int) (v[ii][0] + Math.pow(2, 0) * v[ii][1]);
+
+			for (int jj = 1; jj <= v[ii][2]; jj++) {
+
+				System.out.print(s + " ");
+
+				s += (int) (Math.pow(2, jj) * v[ii][1]);
+
+			}
+
+			System.out.println();
+
+		}
+
 		sc.close();
-	
-		
 	}
 
 }
