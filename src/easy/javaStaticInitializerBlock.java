b222ba2dfaf2cd3b5ef107d997bbf7e40a37cc0d
Leo Abrantes <leoabrantesmelo@gmail.com>
2024-07-24 11:20:41 -0300

    Finishing javaStaticInitializerBlock

--- src/easy/javaStaticInitializerBlock.java
+++ src/easy/javaStaticInitializerBlock.java
@@ -1,10 +1,31 @@
 package easy;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class javaStaticInitializerBlock {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+static Scanner sc = new Scanner(System.in);
 
-	}
+public static int B = sc.nextInt();
+public static int H = sc.nextInt();
+public static boolean flag;
 
+
+static {
+if(B<=0||H<=0) {
+	System.out.print("java.lang.Exception: Breadth and height must be positive");
+	}else {
+		flag= true;}
 }
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
