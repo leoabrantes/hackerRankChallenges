a45ff6133f29bff4b416eea961a94ce34779609b
Leo Abrantes <leoabrantesmelo@gmail.com>
2024-11-20 20:07:56 -0300

    PatternSintaxChecker

---
+++ src/easy/PatternSintaxChecker.java
@@ -0,0 +1,35 @@
+package easy;
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class PatternSintaxChecker {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		String[] str = new String[testCases];
+		
+		for(int i = 0 ; i < testCases ; i++) {
+			String pattern = in.nextLine();
+          	
+			str[i] = pattern;
+		}	
+			
+		for (String s : str) {
+			
+			 try {
+		            Pattern.compile(s);
+		            System.out.println("Valid");
+		        } catch (PatternSyntaxException e) {
+		            System.out.println("Invalid");
+		        }
+			
+		}
+			
+		
+
+		in.close();
+	}
+		
+}
