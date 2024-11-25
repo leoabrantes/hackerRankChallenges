1532cf0c7ca1ab026ee5d6c8eac99cb0c08c2b3d
Leo Abrantes <leoabrantesmelo@gmail.com>
2024-11-24 22:08:34 -0300

    javaInterface.part2

--- src/easy/MyCalculator.java
+++ src/easy/MyCalculator.java
@@ -9,17 +9,33 @@ interface AdvancedArithmetic{
 //Write your code here
 
 
+class MyCalculator implements AdvancedArithmetic {
 
-public class MyCalculator() {
+	@Override
+	public int divisor_sum(int n) {
+		
+		int sum = 0;
+		
+		for(int i=1 ; i <= n; i++) {
+			
+			if(n%i == 0) {
+				
+				sum += i;
+			}
+		}
+		
+		return sum;
+	}
 	
 }
-
 
 
 //Write your code here
 
 
 class JavaInterface{
+
+
     public static void main(String []args){
         MyCalculator my_calculator = new MyCalculator();
         System.out.print("I implemented: ");
@@ -40,3 +56,5 @@ class JavaInterface{
         }
     }
 }
+
+
