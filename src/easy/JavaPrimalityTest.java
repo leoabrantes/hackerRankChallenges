6ded65da86d7a3649c43926f4aa6f42c63923cf5
Leo Abrantes <leoabrantesmelo@gmail.com>
2024-12-04 15:56:34 -0300

    JavaPrimalityTest.part02

--- src/easy/JavaPrimalityTest.java
+++ src/easy/JavaPrimalityTest.java
@@ -8,16 +8,34 @@ import java.util.concurrent.*;
 import java.util.regex.*;
 
 
-
 public class JavaPrimalityTest {
 	
 	public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         String n = bufferedReader.readLine();
+        
+        if(verify(n)) {
+            System.out.print("prime");
+            
+        }else {
+            System.out.println("not prime");
+        }
 
         bufferedReader.close();
+   
     }
-	
-
+    
+    public static boolean verify(String n) {
+        
+        BigInteger prime = new BigInteger(n);        
+        int certainty = 15;
+        boolean verify = prime.isProbablePrime(certainty);
+        if (verify) {
+        return true;
+        }else {
+            return false;
+        }
+    
+    }
 }
