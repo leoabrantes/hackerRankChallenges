b75553c3cdd2b53f436a8fac9027430183a91bc9
Leo Abrantes <leoabrantesmelo@gmail.com>
2025-02-06 04:47:46 -0300

    PrimeChecker

---
+++ src/easy/PrimeChecker.java
@@ -0,0 +1,16 @@
+package easy;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class PrimeChecker {
+	
+	 public static void main(String[] args) {
+		 
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	 
+	 
+	 
+	 }
+
+}
